2505b0f0b85d7bf56af73f41796c915b5ced803a
JoStMc <joshmcdonnell@hotmail.co.uk>
2026-03-14 22:57:12 +0000

    basic http and html setup

--- main.go
+++ main.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load(".env")
+
+
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM environment variable is not set")
+	}
+
+	filepathRoot := os.Getenv("FILEPATH_ROOT")
+	if filepathRoot == "" {
+		log.Fatal("FILEPATH_ROOT environment variable is not set")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	mux := http.NewServeMux()
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", appHandler)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on: http://localhost:%s/app/\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
