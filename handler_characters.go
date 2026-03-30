136a898373fc916f170ad5fcbd80bf5445e58399
JoStMc <joshmcdonnell@hotmail.co.uk>
2026-03-30 19:41:34 +0100

    handler to lookup character added

--- handler_characters.go
+++ handler_characters.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
 
 func (cfg *config) handlerCharacterLookup(w http.ResponseWriter, r *http.Request) {
+	character := r.PathValue("kanji")
 
+	charInfo, err := cfg.dbQueries.GetKanji(context.TODO(), character)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error looking up character: %v", err))
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, charInfo)
 }
