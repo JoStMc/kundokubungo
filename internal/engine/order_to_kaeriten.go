295b8ed850df359b7913d83e9fb6378c4567680f
JoStMc <joshmcdonnell@hotmail.co.uk>
2026-08-13 14:07:03 +0100

    added automatic reten

--- internal/engine/order_to_kaeriten.go
+++ internal/engine/order_to_kaeriten.go
@@ -2,13 +2,16 @@ package engine
 
 import (
 	"errors"
+
+	"github.com/JoStMc/kundokubungo/internal/models"
 )
 
 type kaeri struct {
 	kaeriten []string
 	order []int
 	positions []int
-	sequences []int
+	sequence_ends []int
+	sequence_starts []int
 } 
 
 /* Plan:
@@ -25,9 +28,10 @@ func getKaeriten(order []int) ([]string, error) {
 		return []string{}, err
 	}
 
+
+
 	return k.kaeriten, nil
 } 
-
 
 // Reverses the order array, so the position can be found of each number
 // e.g. if order[4] is 7, positions[7] is 4
@@ -47,14 +51,32 @@ func (k *kaeri) getPositionsAsIndex() error {
 // For example, 143259867 would return a list of the last number in 
 // each descending sequence (of 987, 6, 5, 432, 1) , 7, 6, 5, 2, 1
 func (k *kaeri) getSequences() {
-	sequences := []int{}
+	sequence_starts := []int{}
+	sequence_ends := []int{}
 	current := len(k.order)
 	for current != 0 {
+		sequence_starts = append(sequence_starts, current)
 		for k.positions[current-1] > k.positions[current] {
 		    current -= 1
 		} 
-		sequences = append(sequences, current)
+		sequence_ends = append(sequence_ends, current)
 		current -= 1
 	} 
-	k.sequences = sequences
+	k.sequence_ends = sequence_ends
+	k.sequence_starts = sequence_starts
 } 
+
+// Adds レ to all contiguous descending sequences until there is at least 
+// one character between them. 21 gives 2 レ, 54312 gives 5 and 4 レ
+func (k *kaeri) addRes() {
+	for i, start := range k.sequence_starts {
+		end := k.sequence_ends[i]
+		for current := start; current > end; current-- {
+			if k.positions[current-1] != k.positions[current] + 1 {
+			    break
+			} 
+			k.kaeriten[k.positions[current]] = models.MarkRe
+			k.sequence_starts[i]--
+		} 
+	} 
+}
--- internal/engine/order_to_kaeriten_test.go
+++ internal/engine/order_to_kaeriten_test.go
@@ -27,9 +27,38 @@ func TestGetSequences(t *testing.T) {
 		}
 		k.getSequences()
 
-		if !reflect.DeepEqual(tc.output, k.sequences) {
-			t.Fatalf("%s: expected: %v, got: %v", name, tc.output, k.sequences)
+		if !reflect.DeepEqual(tc.output, k.sequence_ends) {
+			t.Fatalf("%s: expected: %v, got: %v", name, tc.output, k.sequence_ends)
 		} 
 	} 
 } 
 
+
+func TestAddRes(t *testing.T) {
+	tests := map[string]struct {
+		input []int
+		output []int
+	}{
+		"one": {input: []int{1}, output: []int{1}},
+		"in order": {input: []int{1, 2, 3}, output: []int{3, 2, 1}},
+		"reverse order": {input: []int{3, 2, 1}, output: []int{1}},
+		"several": {input: []int{6, 5, 4, 2, 1, 3}, output: []int{4, 1}},
+	}
+
+	for name, tc := range tests {
+		k := kaeri{
+			kaeriten: make([]string, len(tc.input)),
+		    order: tc.input,
+		} 
+		err := k.getPositionsAsIndex()
+		if err != nil {
+			log.Fatal(err)
+		}
+		k.getSequences()
+		k.addRes()
+
+		if !reflect.DeepEqual(tc.output, k.sequence_starts) {
+			t.Fatalf("%s: expected: %v, got: %v", name, tc.output, k.sequence_starts)
+		} 
+	} 
+} 
